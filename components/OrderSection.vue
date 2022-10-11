f26d86b3d760e50b8fe598b6f1586da3c11d6e69
arpado <ozsvar.arpad@gmail.com>
2022-10-11 20:31:14 +0200

    basic visibility to test added

---
+++ components/OrderSection.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container"></div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.container {
+  background-color: pink;
+  width: 100%;
+  height: 100%;
+}
+</style>
