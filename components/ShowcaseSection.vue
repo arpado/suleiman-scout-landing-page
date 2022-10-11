bddff3441aa90495b032b7bf24a32ebcf3bb2e33
arpado <ozsvar.arpad@gmail.com>
2022-10-11 20:31:41 +0200

    basic visibility to test added

--- components/ShowcaseSection.vue
+++ components/ShowcaseSection.vue
@@ -1,13 +1,15 @@
 <template>
-  
+  <div class="container"></div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
+<style scoped>
+.container {
+  background-color: green;
+  width: 100%;
+  height: 100%;
+}
 </style>
